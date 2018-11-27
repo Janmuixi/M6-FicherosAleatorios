7604c1e2e0df6d0d8fd1f61c85f9d2359bd497b8
janmuixi <janmuixi7@gmail.com>
2018-11-27 18:32:23 +0100

    Added consulta, insercion and modificacion

--- src/Modificacion.java
+++ src/Modificacion.java
@@ -17,39 +17,50 @@ public class Modificacion {
 		scan = new Scanner(System.in);
 		int salarioInput = scan.nextInt();	
 		
+		double salarioOldConst = 0;
+		
 		File fichero = new File ("AleatorioEmpleado.dat");
 		RandomAccessFile file = new RandomAccessFile (fichero , "rw");
 		int posicion = 0;
 		int idFinder = 0;
 		char apellido[]= new char[10], aux;
 		long puntero = 0;
+		
 		while(true) {
+			
 			try {
+				
 				file.seek(posicion);
 				idFinder = file.readInt();
 				if (idFinder == idInput) {
+					
 					for ( int i =0; i<apellido.length; i++) {
+						
 						aux = file.readChar(); // Voy leyendo carácter a carácter el apellido y lo guardo
 						apellido[i]=aux; // en el array apellido
+						
 					}
+					
 					String apellidos = new String (apellido);
 					System.out.println(apellidos);
 					file.skipBytes(4);
 					puntero = file.getFilePointer();
 					double salarioOld = file.readDouble();
-					System.out.println(salarioOld);
+					salarioOldConst = salarioOld;
 				}
+				
 				posicion += 36;
+				
 			} catch (EOFException e) {
 				break;
 			}
 			
 			file.seek(puntero);
 			file.writeDouble(salarioInput);
-			System.out.println(salarioInput);
 			
 		}
-		
+		System.out.println("Antiguo sueldo: " + salarioOldConst + "€");
+		System.out.println("Nuevo sueldo: " + salarioInput + "€");
 		
 	}
 
